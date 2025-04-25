78791ba4bcf642d84ca32b0139c22f2dd188cdb8
coon-hound <aaronyou1234@gmail.com>
2025-04-25 21:03:33 +0800

    update

--- src/devices.cpp
+++ src/devices.cpp
@@ -12,9 +12,9 @@ vex::motor       right_motor2(vex::PORT4, vex::ratio6_1, false);
 vex::motor       right_motor3(vex::PORT2, vex::ratio6_1, true);
 
 // Left side motors
-vex::motor       left_motor1(vex::PORT8, vex::ratio6_1, true);
-vex::motor       left_motor2(vex::PORT9, vex::ratio6_1, true);
-vex::motor       left_motor3(vex::PORT7, vex::ratio6_1, false);
+vex::motor       left_motor1(vex::PORT19, vex::ratio6_1, true);
+vex::motor       left_motor2(vex::PORT20, vex::ratio6_1, true);
+vex::motor       left_motor3(vex::PORT17, vex::ratio6_1, false);
 
 // Intake motor
 vex::motor       intake_motor(vex::PORT11, vex::ratio6_1, true);
@@ -26,9 +26,9 @@ vex::motor       lb_motor(vex::PORT10, vex::ratio18_1, false);
 
 // Mogo
 vex::optical     mogo_color(vex::PORT12);
-vex::pneumatics  mogo_piston(brain.ThreeWirePort.A);
+vex::pneumatics  mogo_piston(brain.ThreeWirePort.D);
 
-vex::pneumatics  hang_piston(brain.ThreeWirePort.C);
+vex::pneumatics  hang_piston(brain.ThreeWirePort.B);
 
 // Inertial sensor
 vex::inertial    imu(vex::PORT11);
